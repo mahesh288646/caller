e421ef8fbbf643b90eefd56ca11a08e2fc8a77ee
mahesh288646 <47725130+mahesh288646@users.noreply.github.com>
2022-11-22 00:26:53 -0500

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,17 +1,5 @@
-#!/bin/bash -l
+#!/bin/sh -l
 
-set -e
-
-timestamp() {
-  date -u +"%FT%T.%7NZ"
-}
-
-echo "$(timestamp) - Complie Java App - info - Action Started"
-
-echo "$(timestamp) - Complie Java App - info - Running maven command with following arguments: $INPUT_ARGS"
-
-bash -c "npm $INPUT_ARGS"
-
-#bash -c "chmod -R 777 target"
-
-echo "$(timestamp) - Complie Java App - info - Action Completed"
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
